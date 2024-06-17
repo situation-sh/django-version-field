3cde0e16e1dceafbbdc28d3591191f2a818916c8
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2024-06-17 12:26:38 +0200

    add ability to target a test

--- runtests.py
+++ runtests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # see https://docs.djangoproject.com/en/5.0/topics/testing/advanced/#id3
 
+import argparse
 import os
 import sys
 from pathlib import Path
@@ -13,7 +14,17 @@ BASE_PATH = Path(__file__).parent
 
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
+
+    parser = argparse.ArgumentParser(__file__)
+    parser.add_argument(
+        "target",
+        default="tests",
+        nargs="+",
+        help="Set the test you want to trigger (ex: 'tests.tests.MyTestCase')",
+    )
+    args = parser.parse_args()
+
     django.setup()
     runner_class = get_runner(settings)
-    failures = runner_class().run_tests(["tests"])
+    failures = runner_class().run_tests(args.target)
     sys.exit(bool(failures))
